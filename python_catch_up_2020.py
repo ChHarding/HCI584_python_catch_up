8cbb62f005a67f07f73c5a9e7434a7682b05e22a
Chris Harding <charding@iastate.edu>
2020-05-10 17:30:00 -0500

    outline of topics

---
+++ python_catch_up_2020.py
@@ -0,0 +1,33 @@
+# Python exercises for HCI 584 2020
+
+# basic data types (numbers, strings lists, tuples, dictionaries, sets) 
+
+# numpy
+
+# dataframes (Pandas)
+
+
+# variables
+
+
+# flow control (if, while, for)
+
+
+# modules (import)
+
+
+# Functions/methods
+
+
+# OOP
+
+
+# plotting
+
+
+# files
+
+
+# internet
+
+
