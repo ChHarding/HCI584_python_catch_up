d68fc3b1a2774f315c66b888f6e166a362a6ea5a
Chris Harding <charding@iastate.edu>
2020-05-10 17:53:52 -0500

    converted to structured overview, code in nbs

--- python_catch_up_2020.py
+++ python_catch_up_2020.py
@@ -1,22 +1,35 @@
-# Python exercises for HCI 584 2020
+# Python exercises for HCI 584 2020 - overview
+# Each topic will get its own jupyter notebook)
 
-# basic data types (numbers, strings lists, tuples, dictionaries, sets) 
+# variables
+    # local vs "global"
+    # assignments: copy vs alias
+
+
+# basic data types (bools, numbers, strings lists, tuples, dictionaries, sets) 
+    # for strings and lists: indexing and slices
+    #   
+
 
 # numpy
 
 # dataframes (Pandas)
 
 
-# variables
 
 
 # flow control (if, while, for)
 
 
 # modules (import)
-
+    # namespaces
 
 # Functions/methods
+    # def vs call
+    # args
+        # how do arg values end up 
+    # mention general functions like min, max, len, copy
+    # return value vs in-place change
 
 
 # OOP
