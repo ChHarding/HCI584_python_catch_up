1d34907529f0f7bc8343c127de4f276ed6b129b4
Chris Harding <charding@iastate.edu>
2020-05-19 18:11:26 -0500

    added more about vars, added more files

---
+++ 1) python interpreter.md
@@ -0,0 +1,39 @@
+### python interpreter 
+
+<sup>(Note for Chris: run md preview with Control-Shift-v with Markdown Preview Enhanced VS Plugin)</sup>
+
+
+- python is interpreted
+- you need a python interpreter installed to run any python code
+- if you installed anaconda, the python interpreter executable ("app") is anaconda3/python.exe (win) or anaconda/bin/python (mac) 
+<br />
+
+- the interpreter "eats" code __one line at a time__
+- a python script (.py file) or a jupyter python cell can contain multiple lines, which are run/executed/interpreted from top to bottom
+- typically a script or cell is run via the IDE (Visual Studio code), possible with a debugger
+<br />
+
+- You could also use python in the command line (__terminal__):
+    - open a terminal (or use the TERMINAL in VSC)
+    - To run a script in the same folder e.g. `hello.py`:
+        type `python hello.py` and hit Enter
+    - Run a super short literal script (1-liner):
+        type `python -c "print('hello')"` and hit Enter
+    - Start an interactive session:
+        - type 'python` and hit enter
+        - you'll see `>>>`, which means the interpreter is waiting for you to type in a line of code (interactive python shell)
+        - type `print('hello)'  , then hit return
+        - this is useful if you want to play around or test something
+        - to exit the interactive shell, type `exit()`
+<br/>
+- byte code files  
+    - importing modules can create .pyc files in a local `__pycache__` folder
+    - .pyc files are not compiled but rather .py files "compressed" into [bytecode](https://opensource.com/article/18/4/introduction-python-bytecode)
+    - bytecode is not human readable but substantially smaller than text
+    - it's OK to manually delete `__pycache__` folders, they will be recreated if needed
+
+<!--- Example of running code inside the doc
+```python {cmd=true matplotlib=true}
+print("hello")
+```
+-->
--- 2) variables.py
+++ 2) variables.py
@@ -26,8 +26,82 @@ print(hex(id(var))) # hex address of the (start of) the memory object
 var = ["Now", "I'm", "a", "list!", 1,2,3]
 print(hex(id(var))) # the mem. obj, var refers to now sits at a different address!
 
-# assignments: copy vs alias
+# the earlier string object is now inaccessible, so it will be recycled (garbage collection) 
 
+# compact multi value assignments (essentially unpacking a tuple)
+a, b, c = 1, 2, 3 # short for a tuple (1, 2, 3)
+print(a,b,c)
+
+
+# assignments: copy vs alias
+L1 = [1,2,3] # list (mutable)
+print(hex(id(L1)), L1)
+
+# assigning an existsing variable name to a different variable creates an alias to the same mem. obj.
+L2 = L1
+print(hex(id(L2)), L2) # same address
+
+# changing (mutating) the mem. obj.
+L2[0] = -999
+print(hex(id(L2)), L2) # yep, different content
+print(hex(id(L1)), L1) # L1 (refering to the same mem.obj. also shows the change!
+
+# what happens if one of the 2 references is "deleted"?
+del L1 # "unlinks" L1 from the mem. obj. 
+print(L2) # but as we still have L2 refering to it, it's still alive!
+del L2 # last link is gone, now the memory can be properly released
+
+# How to make a copy?
+L1 = [1,2,3] # list (mutable)
+print(hex(id(L1)), L1)
+
+L2 = L1[:]  # [:] is a slice from beginning to end i.e. all of the list. Same as L1.copy()
+# note on copy() vs deepcopy(): https://docs.python.org/3/library/copy.html 
+print(hex(id(L2)), L2) # same "content", different address
+
+L2[0] = -999
+print(hex(id(L2)), L2) # yep, different content
+print(hex(id(L1)), L1) # as L2 was a copy, and we changed a copy, L1 still shows the same content
+
+# Local variables
+def foo(x):
+    var = x * x # var is a local variable
+    print("local var is", var, hex(id(var)))
+    return var # after the return, var gets deleted
+
+var = 123 # var in a "global" scope
+print("global var is", hex(id(var)))
+
+print(foo(3))  # force use of local var
+print(var) # global var is unchanged
+
+# local variables are critically important for isolating the code inside the function(def part)
+# from code that uses that function (call) from the outside!
+
+# variable names
+# - lower case, use underscores
+# - name should describe the variables role
+# - the more important the variable is the more descriptive and understandable the name should be
+time_to_impact = 12.3
+master_timestamp_deviation_by_type_list = [["Type_a", [1.34, 454.2]], ["Type_b", [4.3, 536.32]]]
+
+# if needed, you can always use an alias for less typing and better comprehension
+m = master_timestamp_deviation_by_type_list # m is a short alias
+print(m[0][0][-1], m[0][1][1]-m[0][1][0], m[1][0][-1], m[1][1][1]-m[1][1][0]) 
+# not pretty but a lot compacter than if I had used master_timestamp_list
+
+# - it's ok to use (very) short names as helpers or within snippets
+
+# - maybe encode the type - if that helps to counter possible confusion
+size_str = input("Enter size: ") # returns a string
+size_num = float(size_str) # convert to int
+print(size_num * 12.3) # do do math we need to use a number, not the string!
+
+
+# constants
+# there's no real way in python to make a read-only variable, so constants are
+# simply indicated via all uppercase names 
+GRAVITY = 9.81
 
 
 
